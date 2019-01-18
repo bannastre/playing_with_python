ef89e8f8492494ae875a919745693420a14962b4
Chris Harrop <chris@jigsaw.xyz>
2019-01-18 14:58:21 +0000

    port config

--- run.sh
+++ run.sh
@@ -5,6 +5,4 @@ echo "Collecting static files"
 python3 ./manage.py collectstatic --noinput
 echo "Starting Django..."
 # python3 ./manage.py runserver
-exec gunicorn mysite.wsgi:application \
-    --bind 0.0.0.0:8000 \
-    --workers 3
+exec gunicorn mysite.wsgi:application
